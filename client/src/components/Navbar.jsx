6390cf9fda198bd0abfae1c17816bc54b3881833
Sourav Chand <souravchand8116586038@gmail.com>
2025-09-20 11:22:13 +0530

    fix: correct logo width in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         src={assets.logo}
         alt="logo"
         
-        className=" w-50 sm:w-44 cursor-pointer"
+        className=" w-40 sm:w-44 cursor-pointer"
         onClick={() => navigate("/")}
       />
 
